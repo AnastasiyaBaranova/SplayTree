2d249e4e44a3b6d1f262c6c448e6e31101811b73
AnastasiyaBaranova <baranova_anastasiya@rambler.ru>
2013-03-11 15:46:25 +0600

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -1,9 +1,8 @@
-
 import junit.framework.TestCase;
 
 public class Tests extends TestCase {
 
-  public void testRemove1() {
+	public void testRemove1() {
 		TreeMap<Integer, Integer> Tree = new TreeMap<Integer, Integer>();
 		Tree.put(1, 9);
 		Tree.put(2, 1);
@@ -14,6 +13,12 @@ public class Tests extends TestCase {
 		assertFalse(!Tree.isEmpty());
 	}
 
+	public void testRemoveFromEmpty() {
+		TreeMap<Integer, Integer> Tree = new TreeMap<Integer, Integer>();
+		assertEquals(null, Tree.remove(2));
+		assertEquals(0, Tree.size());
+	}
+
 	public void testRemove2() {
 		TreeMap<Integer, Integer> Tree = new TreeMap<Integer, Integer>();
 		Tree.put(1, 9);
@@ -21,6 +26,7 @@ public class Tests extends TestCase {
 		Tree.put(3, 5);
 		Tree.remove(2);
 		assertTrue(!Tree.containsKey(2));
+		assertEquals(2, Tree.size());
 	}
 
 	public void testIsEmpty() {
@@ -42,4 +48,31 @@ public class Tests extends TestCase {
 		assertEquals(Tree.size(), 4);
 	}
 
+	public void testSplay1() {
+		TreeMap<Integer, Integer> Tree = new TreeMap<Integer, Integer>();
+
+		Tree.put(1, 7);
+		Tree.put(2, 9);
+		Tree.put(3, 6);
+		Tree.put(4, 1);
+		Tree.put(5, 3);
+		assertEquals(5, Tree.size());
+		Tree.get(3);
+		assertEquals(5, Tree.size());
+
+	}
+
+	public void testSplay2() {
+		TreeMap<Integer, Integer> Tree = new TreeMap<Integer, Integer>();
+
+		Tree.put(1, 7);
+		Tree.put(2, 9);
+		Tree.put(3, 6);
+		Tree.put(4, 1);
+		Tree.put(5, 3);
+		assertEquals(5, Tree.size());
+		Tree.remove(3);
+		assertEquals(4, Tree.size());
+
+	}
 }
